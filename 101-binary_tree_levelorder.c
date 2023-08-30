c278419caf4eddb8eb23f33ea1c0163089a68047
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-08-30 08:20:30 -0700

    function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 1;
+	size_t right_height = 1;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	left_height += binary_tree_height(tree->left);
+	right_height += binary_tree_height(tree->right);
 
 	return (left_height > right_height ? left_height : right_height);
 }
@@ -56,7 +56,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	int height = binary_tree_height(tree);
 	int lvl;
 
-	if (tree == NULL || func == NULL)
+	if (tree == NULL || (*func) == NULL)
 	{
 		return;
 	}
